f3d2c6feb33e462f47ce47e822408602feef98f7
Iryna Pajonchkivska <irynapajonchkivska@gmail.com>
2024-03-21 19:59:08 +0200

    updates

--- js/burger.js
+++ js/burger.js
@@ -1,2 +1,6 @@
-
-
+let burgerMenu = document.querySelector('.burger-menu');
+        let menu = document.querySelector('.menu');
+        burgerMenu.addEventListener('click', () => {
+          menu.classList.toggle('is-active');
+          burgerMenu.classList.toggle('is-active');
+        })
